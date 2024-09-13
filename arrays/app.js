c0b75da6bd871e33387db69386b4871b02c739a5
AdarshaCoding <adarshapc4467@gmail.com>
2024-09-13 21:03:02 +0530

    Arrays - DSA in JavaScript

---
+++ arrays/app.js
@@ -0,0 +1,9 @@
+// Arrays
+
+const arr = [1, 2, 3, 4, 5, 6];
+console.log(arr);
+
+// Traversal: Iterating through the elements of an array
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
