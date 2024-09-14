176a0a1d1eddf905b75e328c293d4928ed492206
AdarshaCoding <adarshapc4467@gmail.com>
2024-09-14 06:24:11 +0530

    Arrays - DSA in JavaScript: Bubble Sort_Iterations details

--- arrays/sorting/bubble_sort.js
+++ arrays/sorting/bubble_sort.js
@@ -1,20 +1,26 @@
-const arr = [10, 40, 20, 15, 7, 57];
+const arr = [10, 40, 20, 57, 7, 15];
 str = "";
 for (let i = 0; i < arr.length; i++) {
   str += arr[i] + " ";
 }
 console.log(str);
 
+// this loop is for iterations (based on the size of the array)
 for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr.length - i - 1; j++) {
+  //this loop used to swap the elements
+  for (let j = 0; j < arr.length; j++) {
     //arr.length - i - 1 : reducing the iterations on each step
     // ascending order: arr[j] > arr[j + 1]   descending order: arr[j] < arr[j + 1]
+    console.log(
+      `i, j, aar[i], arr[j], iteration - ${i + 1} : ${i}, ${j} - ${arr[i]} - ${
+        arr[j]
+      }`
+    );
     if (arr[j] > arr[j + 1]) {
       temp = arr[j];
       arr[j] = arr[j + 1];
       arr[j + 1] = temp;
     }
   }
+  console.log(arr);
 }
-
-console.log(arr);
